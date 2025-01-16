f9ea9181a645abe6cb2b6fbc02d4e12cf81c1941
junbin <junbin2438@naver.com>
2025-01-16 17:15:01 +0900

    movieItem 구현

---
+++ app/src/main/java/com/junbean/movieapp/ui/components/movie/MovieItem.kt
@@ -0,0 +1,73 @@
+package com.junbean.movieapp.ui.components.movie
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+private val CARD_WIDTH = 150.dp
+
+@Composable
+fun MovieItem() {
+    Column(
+        modifier = Modifier
+            .width(CARD_WIDTH)
+            .padding(10.dp)
+    ) {
+        Poster(
+            modifier = Modifier.width(CARD_WIDTH)
+        )
+
+        Text(
+            text = "The Lord the Ring 1",
+            maxLines = 1,                       // 최대 1줄 지정
+            overflow = TextOverflow.Ellipsis,   // 1줄이 넘어가면 끝을 ...으로 지정
+            modifier = Modifier.padding(
+                top = 11.dp
+            )
+        )
+
+        Row(
+            modifier = Modifier.padding(
+                vertical = 10.dp
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "5.0"
+            )
+        }
+    }
+}
+
+@Composable
+fun Poster(modifier: Modifier) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+    ) {
+        Box(
+            modifier = Modifier.background(Color.Blue)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMovieItem() {
+    MovieItem()
+}
